834a29da108d541ed48e3133dd4870aff6bf1a64
paulD-isekai <paul.delerue@student.junia.com>
2024-10-14 16:55:12 +0200

    Test.specs parametrage

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,10 +1,15 @@
 import { test } from './fixtures'
 
+
 test.beforeEach(async({page}) => {
- await page.goto('http://localhost:4200/')
- await page.getByText('Forms').click()
- await page.getByText('Form Layouts').click()
+ await page.goto('https://www.amazon.fr/')
+ await page.locator('nb-card').filter({ hasText: 'Accepter' }).getByRole('button').click()
+
  })
-test('Form Layouts page', async({FormLayouts}) => {
- await FormLayouts.BasicForm()
-});
+//test('Form Layouts page', async({FormLayouts}) => {
+ //await FormLayouts.BasicForm()
+//});
+
+test('Changer la langue du site', async({FormLayouts})=>{
+   // await FormLayouts.LangueForm()
+})
