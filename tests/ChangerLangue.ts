8dead75ffb1dc019c1dc9a9254b0b09c3e0e6613
paulD-isekai <paul.delerue@student.junia.com>
2024-10-15 22:04:29 +0200

    aa

--- tests/ChangerLangue.ts
+++ tests/ChangerLangue.ts
@@ -1,19 +1,15 @@
 import { Locator, Page, expect } from "@playwright/test"
 import {faker} from '@faker-js/faker'
 
-export class ChangerLangue {
+export class MeilleuresVentes {
     readonly page: Page;
-    readonly hamburgerMenu: Locator;
-    readonly LienMeilleuresVentes: Locator;
     
     constructor(page: Page){
         this.page = page
-        this.hamburgerMenu = page.locator('#nav-hamburger-menu')
-        this.LienMeilleuresVentes = page.locator('a').filter({hasText : "Meilleures ventes"})
+       
     }
 
     async BasicForm() {
-        await this.hamburgerMenu.click()
-        await this.LienMeilleuresVentes.click()
+        await this.page.locator("#ip-nav-flyout").click()
     }
 }
