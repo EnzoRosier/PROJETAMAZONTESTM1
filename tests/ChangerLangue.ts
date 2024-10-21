b12fb81748015cefd2e131bb05a38ed0099e683c
paulD-isekai <paul.delerue@student.junia.com>
2024-10-21 15:06:30 +0200

    changerLangueExpect

--- tests/ChangerLangue.ts
+++ tests/ChangerLangue.ts
@@ -14,6 +14,6 @@ export class ChangerLangue {
         await this.page.click('input[type="radio"][value="en_GB"]', { force: true });
         const isChecked = await this.page.isChecked('input[type="radio"][value="en_GB"]');
         await this.page.locator('[aria-labelledby="icp-save-button-announce"]').click()
-        await this.page.locator(".icp-nav-link-inner").click()
+        await expect(this.page.locator('html')).toHaveAttribute('lang', 'en-gb');
     }
 }
