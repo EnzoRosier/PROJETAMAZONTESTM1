a3710adaa7e61cd6bbc0d9112fd4b63d88d998fe
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2024-10-21 14:38:38 +0200

    Premier test qui marche hourra !

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,7 +2,7 @@ import { test } from './fixtures'
 
 
 test.beforeEach(async({page}) => {
- await page.goto('https://www.amazon.fr/') // le lidne de amaezon
+ await page.goto('https://www.amazon.fr/') // le lien d'amazon
  if( await page.locator("#sp-cc-accept")!=undefined){
     await page.locator("#sp-cc-accept").click() // permet de passer l'étape des cookies
  }
