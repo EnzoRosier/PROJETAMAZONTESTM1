db00b41e5871b9d42237e734637efdfdc6e077c2
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2024-10-21 15:33:29 +0200

    Dernières Nouveautés finito

--- tests/DerniereNouveautes.ts
+++ tests/DerniereNouveautes.ts
@@ -3,18 +3,23 @@ import {faker} from '@faker-js/faker'
 
 export class DernieresNouveautes {
     readonly page: Page;
-    readonly lienMeilleuresVentes: Locator;
-    readonly changerPageHighTech : Locator;
+    readonly lienDernieresNouveautes: Locator;
+    readonly lienBebe : Locator;
+    readonly lienTop1 : Locator;
     
     constructor(page: Page){
         this.page = page
-        this.lienMeilleuresVentes = page.locator(".nav-a").filter({hasText : "Meilleures ventes"});
-        this.changerPageHighTech = page.locator("#a-autoid-1");
+        this.lienDernieresNouveautes = page.locator(".nav-a").filter({hasText : "Dernières Nouveautés"});
+        this.lienBebe = page.locator("._p13n-zg-nav-tree-all_style_zg-browse-item__1rdKf").filter({hasText : "Bébé et Puériculture"});
+        this.lienTop1 = page.locator("#gridItemRoot").first();
     }
 
     async BaseTest() {
-        await this.lienMeilleuresVentes.click();
-        await this.changerPageHighTech.click();
-        await expect(await this.page.url()).toEqual("https://www.amazon.fr/gp/bestsellers/?ref_=nav_cs_bestsellers")
+        await this.lienDernieresNouveautes.click();
+        await this.lienBebe.click();
+        await expect(await this.page.url()).toEqual("https://www.amazon.fr/gp/new-releases/baby/ref=zg_bsnr_nav_baby_0")
+        await this.lienTop1.click();
+        // Comme les articles des dernières nouveautés changent souvent on ne peut que tester qu'on a bien quitté la page
+        await expect(await this.page.url()!="https://www.amazon.fr/gp/new-releases/baby/ref=zg_bsnr_nav_baby_0").toBeTruthy()
     }
 }
