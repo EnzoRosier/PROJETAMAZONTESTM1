37b8f01ed45a0e27860b3b60ab02b5d6f6b822c8
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2024-10-30 17:10:51 +0100

    La PS5 est là

--- tests/ChercherPS5.ts
+++ tests/ChercherPS5.ts
@@ -6,18 +6,25 @@ export class ChercherPS5 {
     readonly hamburgerMenu : Locator;
     readonly sectionJeux : Locator;
     readonly lienPS5 : Locator;
+    readonly voirPlus : Locator;
+    readonly verif : Locator
     
     constructor(page: Page){
         this.page = page
         this.hamburgerMenu = page.locator("#nav-hamburger-menu");
         this.sectionJeux = page.getByRole('link', { name: 'Jeux vidéo et Consoles' })
-        this.lienPS5 = page.getByRole('link', { name: 'PlayStation 5' }).first();
+        this.lienPS5 = page.getByRole('link', { name: 'PlayStation 5'}).first();
+        this.voirPlus = page.getByRole('link', { name: 'Voir plus Les plus demandés' })
+        this.verif = page.getByText('Les plus demandés', { exact: true })
     }
 
     async BaseTest() {
         await this.hamburgerMenu.click();
         await this.sectionJeux.click();
-        await this.lienPS5.click();
-        await this.page.waitForTimeout(3000);
+        await this.lienPS5.click({force : true});
+        await expect(await this.page.url()=="https://www.amazon.fr/gp/browse.html?node=20904281031&ref_=nav_em_ps5_0_2_11_8").toBeTruthy()
+        await this.voirPlus.click({force : true});
+        await expect(await this.verif).toBeVisible()
+        await this.page.waitForTimeout(1000);
     }
 }
